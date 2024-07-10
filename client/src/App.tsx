b2c23c1ee6d35cf47ae54fafa8ac9cf5f22a8db0
Karthikeya <veruturikarthikeya@gmail.com>
2024-07-10 21:00:04 +0530

    firebase down

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,15 +7,22 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 const App: React.FC = () => {
   return (
-    <div className='w-screen h-screen flex flex-col'>
-      <Toaster />
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/:alias' element={<RedirectHandler />} /> 
-        </Routes>
-      </Router>
+    // <div className='w-screen h-screen flex flex-col'>
+    //   <Toaster />
+    //   <Router>
+    //     <Navbar />
+    //     <Routes>
+    //       <Route path='/' element={<Home />} />
+    //       <Route path='/:alias' element={<RedirectHandler />} /> 
+    //     </Routes>
+    //   </Router>
+    // </div>
+    <div>
+      app under service! sorry for the inconvenience
+      <br />
+      meanwhile, you can check my other projs at 
+      <a href="https://kv3.vercel.app">link</a>
+
     </div>
   );
 }
